bd6672de0f0ff77a57fb3650d9fe1e32d0926e4e
sheetal patil <patilsheetal7913@gmail.com>
2022-08-17 17:12:57 +0530

    welcome to employeewage

--- employeewage.sh
+++ employeewage.sh
@@ -4,14 +4,16 @@ echo "welcome to employeewage programme."
 isparttime=1;
 isfulltime=2;
 emprateperhr=20;
-randomcheck=$((RANDOM%3))
+randomcheck=$((RANDOM%3));
 
-if '[ $isfulltime -eq $randomcheck ]'
+if [ $isfulltime -eq $randomcheck ]
 then
     emphrs=16;
-elif '[ $isparttime -eq $randomcheck ]'
+elif [ $isparttime -eq $randomcheck ]
 then
     emphrs=8;
+else
+   emphrs=0;
 fi
 salary=$(($emphrs*$emprateperhr))
 
