d887c7bca02896630db2f3fd634546be8afda621
sheetal patil <patilsheetal7913@gmail.com>
2022-08-17 12:47:01 +0530

    welcome to employeewage.

--- employeewage.sh
+++ employeewage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "welcome to employeewage programme."
+ispresent=1
+randomcheck=$((RANDOM%2))
+
+if [ $ispresent -eq $randomcheck ]
+then
+echo "employee is present."
+else
+echo "employee is absent."
+fi
